f8ed297e42befdafc80f3928f8a1f22a748b2ac6
stevencschoi <stevencschoi87@gmail.com>
2020-03-16 17:44:09 -0400

    scheduler with conditional button styling

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,32 @@
 import React from "react";
 
+const classNames = require("classNames");
+
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+  const buttonClass = classNames("button", {
+    "button--confirm": props.confirm,
+    "button--danger": props.danger
+  });
+
+  //   let buttonClass = "button";
+
+  //   if (props.confirm) {
+  //     buttonClass += " button--confirm";
+  //   }
+
+  //   if (props.danger) {
+  //     buttonClass += " button--danger";
+  //   }
+
+  return (
+    <button
+      className={buttonClass}
+      onClick={props.onClick}
+      disabled={props.disabled}
+    >
+      {props.children}
+    </button>
+  );
 }
