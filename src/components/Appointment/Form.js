4b4e634a3cf823ab1f2a7190a4be2bd2b9d8666a
stevencschoi <stevencschoi87@gmail.com>
2020-03-17 21:47:20 -0400

    completed forms

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,22 +1,33 @@
 import React, { useState } from "react";
 import InterviewerList from "../InterviewerList";
 import Button from "../Button";
-
 export default function Form(props) {
-  const [name, setName] = useState("");
-  const [interviewer, setInterviewer] = useState(null);
+  const [name, setName] = useState(props.name || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  const reset = () => {
+    setName("");
+    setInterviewer(null);
+  };
+
+  const cancel = props => {
+    reset();
+    props.onCancel();
+  };
 
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form onSubmit={event => event.preventDefault()} autoComplete="off">
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
             type="text"
             placeholder="Enter Student Name"
-            value={props.name}
-            onChange={event => setName(event)}
+            value={name}
+            onChange={e => {
+              setName(e.target.value);
+            }}
           />
         </form>
         <InterviewerList
@@ -27,8 +38,12 @@ export default function Form(props) {
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger>Cancel</Button>
-          <Button confirm>Save</Button>
+          <Button danger onClick={cancel}>
+            Cancel
+          </Button>
+          <Button confirm onClick={props.onSave}>
+            Save
+          </Button>
         </section>
       </section>
     </main>
