83c93887682ee0bb4912a4979b2334a03f3aa48a
stevencschoi <stevencschoi87@gmail.com>
2020-03-19 13:41:15 -0400

    added fn getInterview

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,29 +5,32 @@ import "components/Application.scss";
 
 import DayList from "components/DayList";
 import Appointment from "components/Appointment";
-import getAppointmentsForDay from "helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
 export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
 
   const setDay = day => setState({ ...state, day });
-  const setDays = days => setState(prev => ({ ...prev, days }));
+  // const setDays = days => setState(prev => ({ ...prev, days }));
 
   useEffect(() => {
     Promise.all([
-      axios.get("http://localhost:8001/api/days"),
-      axios.get("http://localhost:8001/api/appointments")
+      axios.get("api/days"),
+      axios.get("api/appointments"),
+      axios.get("api/interviewers")
     ])
       .then(response => {
         console.log(response);
         setState(prev => ({
           ...prev,
           days: response[0].data,
-          appointments: response[1].data
+          appointments: response[1].data,
+          interviewers: response[2].data
         }));
       })
       .catch(err => {
@@ -37,6 +40,19 @@ export default function Application(props) {
 
   // why does this work???
   const appointments = getAppointmentsForDay(state, state.day);
+
+  const schedule = appointments.map(appointment => {
+    const interview = getInterview(state, appointment.interview);
+
+    return (
+      <Appointment
+        key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interview={interview}
+      />
+    );
+  });
 
   return (
     <main className="layout">
@@ -57,11 +73,14 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {appointments.map(appointment => {
-          return <Appointment key={appointment.id} {...appointment} />;
-        })}
+        {schedule}
         <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
 }
+
+// inside <section className="schedule">
+// {appointments.map(appointment => {
+//   return <Appointment key={appointment.id} {...appointment} />;
+// })}
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -3,8 +3,9 @@
 //   return filteredNames;
 // }
 
-export default function getAppointmentsForDay(state, day) {
+function getAppointmentsForDay(state, day) {
   const daysArray = [];
+  // if day.name of daysArray[i], get that day object
   for (const aptDay of state.days) {
     if (aptDay.name === day) {
       daysArray.push(aptDay);
@@ -15,10 +16,10 @@ export default function getAppointmentsForDay(state, day) {
     return [];
   }
 
-  const aptArr = daysArray[0].appointments;
-
+  const aptArr = daysArray[0].appointments; // apt ids for day
   const dayApts = [];
 
+  // if items in array id matches appointment obj, show
   for (const appointment of Object.values(state.appointments)) {
     if (aptArr.includes(appointment.id)) {
       dayApts.push(appointment);
@@ -26,3 +27,21 @@ export default function getAppointmentsForDay(state, day) {
   }
   return dayApts;
 }
+
+// return obj if passed in an obj that contains interviewer
+function getInterview(state, interview) {
+  if (interview === null || !interview) {
+    return null;
+  }
+
+  for (const interviewer of Object.values(state.interviewers)) {
+    if (interview.interviewer === interviewer.id) {
+      return { student: interview.student, interviewer: interviewer };
+    }
+  }
+}
+
+module.exports = {
+  getAppointmentsForDay,
+  getInterview
+};
